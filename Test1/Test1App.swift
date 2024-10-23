5e9f3ed76d63fbf71f5ad73d96bf25748513fe9c
schlaurelio <97165507+schlaurelio@users.noreply.github.com>
2024-10-23 23:58:53 +0200

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Aurelio D'Amato on 23.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Test1Tests/Test1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Test1Tests.swift
+//  Test1Tests
+//
+//  Created by Aurelio D'Amato on 23.10.2024.
+//
+
+import Testing
+@testable import Test1
+
+struct Test1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
